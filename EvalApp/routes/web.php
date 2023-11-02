dfa4dd7c216b7b2946a98c576e216354de6eb826
mr-jmd <juan.amayacadavid@gmail.com>
2023-11-01 20:55:21 -0500

    Cambio de logo

--- EvalApp/routes/web.php
+++ EvalApp/routes/web.php
@@ -3,20 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
