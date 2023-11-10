aac8f4eae8e4542380657c52d9969a3ce04b1f0e
JMA <juan.amaya@sothis.tech>
2023-11-10 12:36:39 -0500

    add Projects

--- EvalApp/app/Http/Requests/StoreProjectsRequest.php
+++ EvalApp/app/Http/Requests/StoreProjectsRequest.php
@@ -22,11 +22,29 @@ class StoreProjectsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'id' => ['required', 'unique:projects'],
-            'Name' => ['required', 'string', 'max:255'],
-            'Percentage_Completion' => ['required', 'string', 'max:20'],
-            'Contract_Id' => ['required', 'string', 'max:255'],
-            'State_Id' => ['required', 'string', 'max:255'],
+            'Name' => 'required|string|max:255',
+            'Percentage_Completion' => 'required|numeric|between:0,100',
+            'Contract_Id' => 'required|exists:contract,id',
+            'State_Id' => 'required|exists:state,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'Name.required' => 'El campo nombre del proyecto es obligatorio.',
+            'Name.string' => 'El nombre del proyecto debe ser una cadena de texto.',
+            'Name.max' => 'El nombre del proyecto no puede tener más de :max caracteres.',
+
+            'Percentage_Completion.required' => 'El campo porcentaje de completado es obligatorio.',
+            'Percentage_Completion.numeric' => 'El porcentaje de completado debe ser un valor numérico.',
+            'Percentage_Completion.between' => 'El porcentaje de completado debe estar entre :min y :max.',
+
+            'Contract_Id.required' => 'El campo ID de contrato es obligatorio.',
+            'Contract_Id.exists' => 'El ID de contrato seleccionado no existe en la tabla de contratos.',
+
+            'State_Id.required' => 'El campo ID de estado es obligatorio.',
+            'State_Id.exists' => 'El ID de estado seleccionado no existe en la tabla de estados.',
         ];
     }
 
